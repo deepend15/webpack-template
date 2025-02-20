c0f6ecb1c4d2a3d620010a162cc1223e8db80f32
deepend15 <170750487+deepend15@users.noreply.github.com>
2025-02-20 13:27:45 -0500

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+};
